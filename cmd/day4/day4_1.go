9dcbe53d79a9dab1baa75856be3f3fa637115df5
Ferozaffs <zmass@ZMASS>
2023-12-04 21:49:38 +0100

    completed day 4

--- cmd/day4/day4_1.go
+++ cmd/day4/day4_1.go
@@ -2,6 +2,14 @@ package day4
 
 import (
 	"aoc2023/cmd"
+	"bufio"
+	"fmt"
+	"math"
+	"os"
+	"regexp"
+	"slices"
+	"strconv"
+	"strings"
 
 	"github.com/spf13/cobra"
 )
@@ -20,9 +28,39 @@ func init() {
 }
 
 func Run1() {
+	dat, _ := os.ReadFile("inputs/day4_data.txt")
+	ans := Solve1(string(dat))
 
+	fmt.Printf("ANSWER: %d\n", ans)
 }
 
-func Solve1() {
+func Solve1(data string) int {
+	sum := 0
 
+	scanner := bufio.NewScanner(strings.NewReader(data))
+	for scanner.Scan() {
+		s := scanner.Text()
+		index := regexp.MustCompile(`\|`).FindStringIndex(s)
+
+		winningNumbers := []int{}
+		matches := 0
+		for i, m := range regexp.MustCompile(`\d+`).FindAllStringIndex(s, -1) {
+			//Skip card ID
+			if i == 0 {
+				continue
+			}
+
+			n, _ := strconv.Atoi(s[m[0]:m[1]])
+			if m[0] < index[0] {
+				winningNumbers = append(winningNumbers, n)
+			} else if slices.Contains(winningNumbers, n) {
+				matches++
+			}
+		}
+
+		sum += int(math.Pow(2, float64(matches-1)))
+
+	}
+
+	return sum
 }
--- cmd/day4/day4_2.go
+++ cmd/day4/day4_2.go
@@ -2,6 +2,13 @@ package day4
 
 import (
 	"aoc2023/cmd"
+	"bufio"
+	"fmt"
+	"os"
+	"regexp"
+	"slices"
+	"strconv"
+	"strings"
 
 	"github.com/spf13/cobra"
 )
@@ -20,8 +27,46 @@ func init() {
 }
 
 func Run2() {
+	dat, _ := os.ReadFile("inputs/day4_data.txt")
+	ans := Solve2(string(dat))
+
+	fmt.Printf("ANSWER: %d\n", ans)
 }
 
-func Solve2() {
+func Solve2(data string) int {
+	sum := 0
 
+	lines := []string{}
+	scanner := bufio.NewScanner(strings.NewReader(data))
+	for scanner.Scan() {
+		lines = append(lines, scanner.Text())
+	}
+
+	for i := 0; i < len(lines); i++ {
+		index := regexp.MustCompile(`\|`).FindStringIndex(lines[i])
+
+		winningNumbers := []int{}
+		matches := 0
+		card := 0
+		for j, m := range regexp.MustCompile(`\d+`).FindAllStringIndex(lines[i], -1) {
+			//Skip card ID
+			if j == 0 {
+				card, _ = strconv.Atoi(lines[i][m[0]:m[1]])
+				continue
+			}
+
+			n, _ := strconv.Atoi(lines[i][m[0]:m[1]])
+			if m[0] < index[0] {
+				winningNumbers = append(winningNumbers, n)
+			} else if slices.Contains(winningNumbers, n) {
+				matches++
+			}
+		}
+		for j := 0; j < matches; j++ {
+			lines = append(lines, lines[card+j])
+		}
+		sum++
+	}
+
+	return sum
 }
